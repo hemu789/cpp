09644a063bf467974b16fda040ae154981c6176c
hemu789 <87188130+hemu789@users.noreply.github.com>
2022-11-09 08:34:11 +0530

    arr

---
+++ Arrays/findUniq.cpp
@@ -0,0 +1,42 @@
+// CODE STUDIO
+// Single element print n remaining are repeatable
+
+// 7 2 5 3 2 5 7
+//  o/p: 3
+
+#include <iostream>
+using namespace std;
+
+void takeInput(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    takeInput(arr, n);
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    cout << ans;
+    return 0;
+}
