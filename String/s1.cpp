277151a7e5bb033f6750d1103888246d0b2529d8
hemu789 <87188130+hemu789@users.noreply.github.com>
2022-11-10 16:15:31 +0530

    ans

--- String/s1.cpp
+++ String/s1.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 int main(){
 
-    char x='a';
-    cout<<int(x);
+    // char x='a';
+    // cout<<int(x);
     
+    string s="geeksforgeeks";
+    int count[26]={0};
+
+    for(int i=0;i<s.size();i++){
+        count[s[i]-'a']++;
+
+        // g-a -> 6 index 
+    }
+
+    for(int i=0;i<26;i++){
+
+        if(count[i]> 0){
+
+            cout<<(char)(i+'a')<<" "<< count[i] <<endl;
+        }
+    }
     return 0;
 }
