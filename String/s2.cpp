3d27bc29e7c943627e56184b2a6d9b3d8f92990b
hemu789 <87188130+hemu789@users.noreply.github.com>
2022-11-10 16:46:34 +0530

    update

---
+++ String/s2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // char str[6];
+    // str="gfg";
+
+    // // str is a address , not possible
+    // cout<<str<<" ";
+
+    // so that/s why use strcpy
+
+    char str[6];
+    strcpy(str, "gfg");
+    cout << str << " ";
+
+    // int arr[5];
+    // arr[1]=2;
+    // applicable
+
+    return 0;
+}
